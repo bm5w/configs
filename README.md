15f48a264bc3f297de10babb4f3b7619f6d3af1e
Mark Saiget <saiget@gmail.com>
2016-02-28 13:38:04 -0800

    Add fish config files and update readme.

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# config
+# configs
 .vimrc file for vim configuration
+ln -s ~/projects/configs/.vimrc .vimrc
 
 iterm2:
 solarized dark theme
 
 fish configuration:
+config.fish -> ~/.config/fish/ 
+ln -s ~/.config/fish/config.fish config.fish
+fish_prompt.fish -> ~/.config/fish/functions/
+ln -s ~/.config/fish/functions/fish_prompt.fish fish_prompt.fish
+
---
+++ config.fish
@@ -0,0 +1,23 @@
+# Sublime Text
+set -gx EDITOR 'subl -w'
+
+# virtualfish
+source ~/.virtualfish/virtual.fish
+source ~/.virtualfish/projects.fish
+
+# activate builtin plugins
+eval (python -m virtualfish auto_activation global_requirements)
+
+# exec (python -m virtualfish auto_activation global_requirements compat_aliases)
+
+# pyenv
+# set -x pyenvshortcut
+# eval "$(pyenv init -)"
+
+function mkdirc
+    mkdir $argv
+    cd $argv
+end
+
+# vim in shell
+set fish_key_bindings fish_vi_key_bindings
