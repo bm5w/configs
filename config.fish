17edf3c610dc5d3a663206990a2e7a0153d52ff8
Mark Saiget <saiget@gmail.com>
2019-05-16 20:17:55 -0700

    Add ruby and go paths.

--- config.fish
+++ config.fish
@@ -21,3 +21,12 @@ end
 # vim in shell
 set fish_key_bindings fish_vi_key_bindings
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
+
+# add ssh key
+ssh-add ~/.ssh/id_rsa
+
+# Go development
+set -x GOPATH $HOME/golang
+set -x GOROOT /usr/local/opt/go/libexec
+set PATH $GOPATH/bin $GOROOT/bin $PATH
